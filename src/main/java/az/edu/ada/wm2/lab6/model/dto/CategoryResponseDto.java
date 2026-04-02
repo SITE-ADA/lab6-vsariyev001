e8e8b03958009d2f999adf9482a5ca49bf2b6872
vsariyev001 <veyselsariyev.az@gmail.com>
2026-04-02 16:36:10 +0400

    Add DTOs

---
+++ src/main/java/az/edu/ada/wm2/lab6/model/dto/CategoryResponseDto.java
@@ -0,0 +1,19 @@
+package az.edu.ada.wm2.lab6.model.dto;
+
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryResponseDto {
+
+    private UUID id;
+    private String name;
+}
